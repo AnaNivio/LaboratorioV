6d5795eede8802d2f03f4f9a3132cc744e46a0ef
Ana <ana_laura-99@hotmail.com>
2019-06-23 14:23:13 -0300

    "PlayerService created; use of Executor in Player's components"

---
+++ PracticaParcial2/src/main/java/com/example/PracticaParcial/interfaces/PlayersMonthsInTeam.java
@@ -0,0 +1,9 @@
+package com.example.PracticaParcial.interfaces;
+
+public interface PlayersMonthsInTeam {
+
+    String getName();
+    String getSurname();
+    Integer getAge();
+    Integer getMonthsInTeam();
+}
