9ec1bf99a13417f1f00fda637fca5b68e58ccf22
AnaNivio <ana_laura-99@hotmail.com>
2019-06-23 14:29:11 -0300

    Add files via upload

---
+++ PracticaParcial2/src/main/java/com/example/PracticaParcial/controller/PlayerController.java
@@ -0,0 +1,85 @@
+package com.example.PracticaParcial.controller;
+
+import com.example.PracticaParcial.interfaces.PlayersMonthsInTeam;
+import com.example.PracticaParcial.models.Player;
+import com.example.PracticaParcial.repositories.TeamRepository;
+import com.example.PracticaParcial.service.PlayerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.HttpClientErrorException;
+
+import javax.validation.Valid;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+
+@RestController
+@RequestMapping("/player")
+public class PlayerController {
+
+    @Autowired
+    private PlayerService playerService;
+    @Autowired
+    private TeamRepository teamRepository;
+
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping("")
+    public void createPlayers(@RequestBody @Valid Player player){
+
+        try {
+            playerService.addPlayer(player);
+
+        } catch (DataIntegrityViolationException e) {
+            throw new HttpClientErrorException(HttpStatus.INTERNAL_SERVER_ERROR,String.format("An error has occurred while the player was created"));
+        }
+
+    }
+
+    @ResponseStatus(HttpStatus.OK)
+    @GetMapping("")
+    public List<Player> getAllPlayres() {
+
+        List<Player> foundPlayers = playerService.getPlayers();
+
+        if(foundPlayers.isEmpty()){
+           throw  new HttpClientErrorException(HttpStatus.NO_CONTENT,"There aren't any players uploaded yet");
+        }
+        return foundPlayers;
+    }
+
+    @GetMapping("/proyection")
+    public ResponseEntity<List<PlayersMonthsInTeam>> getMonthPlayers() {
+        CompletableFuture<List<PlayersMonthsInTeam>> result= playerService.getPlayersMonthInTeam();
+
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(result.join());
+    }
+
+   /* @ResponseStatus(HttpStatus.OK)
+    @GetMapping("/{id}")
+    public Player getPlayerById(@PathVariable Integer id) {
+        return playerRepository.findById(id).orElseThrow(()->new HttpClientErrorException(HttpStatus.BAD_REQUEST, String.format("The player with this id doesn't exist: %s",id)));
+    }
+
+
+
+    @ResponseStatus(HttpStatus.OK)
+    @PutMapping("")
+    public void updatePlayers(@RequestBody @Valid Player player){
+        playerRepository.findById(player.getIdPlayer()).orElseThrow(()->new HttpClientErrorException(HttpStatus.BAD_REQUEST, String.format("The player doesn't exist")));
+        playerRepository.save(player);
+    }
+
+    @ResponseStatus(HttpStatus.OK)
+    @DeleteMapping("/{id}")
+    public void deletePlayers(@PathVariable Integer idPlayer){
+        Player playerFound = playerRepository.findById(idPlayer).orElseThrow(()->new HttpClientErrorException(HttpStatus.BAD_REQUEST, String.format("The player doesn't exist")));
+
+        playerRepository.delete(playerFound);
+    }
+
+*/
+}
